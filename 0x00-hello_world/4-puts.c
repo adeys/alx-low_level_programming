decedf9c31600a5f65344f9a8e45c5c46957af02
adeys <raphalg@duck.com>
2022-03-11 02:50:48 +0100

    Fixed betty non-compliant syntax

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point
+ * Prints a string using "puts"
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
-    return 0;
+/**
+ * main - Entry point
+ * Prints a string using "printf"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
