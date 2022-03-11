6f9cdff2038cb7c36268b300afdc11ad4d9a295b
adeys <raphalg@duck.com>
2022-03-11 03:42:47 +0100

    Alternative to printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Prints a string to stderr
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	fwrite(
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		sizeof(char),
+		59,
+		stderr
+	);
+	return (1);
+}
