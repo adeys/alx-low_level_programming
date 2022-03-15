b72a3289c5d4c24846587aa119c7cb9077d411e6
adeys <raphalg@duck.com>
2022-03-15 22:38:39 +0100

    Task 6: Absolute value function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to use
+ *
+ * Return: The @n's absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
