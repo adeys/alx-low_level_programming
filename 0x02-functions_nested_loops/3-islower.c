84c3a99fad51beec9d1c75c6d57056f105456c65
adeys <raphalg@duck.com>
2022-03-15 21:54:42 +0100

    Task 3: islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+/**
+ * _islower - Check for lowercase character
+ * @c: The character to check
+ * Return: 1 if @c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
