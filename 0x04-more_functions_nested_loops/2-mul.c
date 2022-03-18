160ab72ef5bd48890580d8840aad719dc100c650
adeys <raphalg@duck.com>
2022-03-18 10:35:10 +0100

    Task 2: Multiplication function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul -  Multiplies two integers
+ * @a: The first number to multiply
+ * @b: The second number to multiply
+ * Return: The value of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
