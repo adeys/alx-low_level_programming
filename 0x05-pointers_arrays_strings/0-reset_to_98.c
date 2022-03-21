eb8a5983b825f828da57b6bf7f5c525f1f476376
adeys <raphalg@duck.com>
2022-03-21 21:19:10 +0100

    Task 0: 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets the value of a pointer to 98
+ * @n: Pointer to the value to reset
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# Project 0x05: Pointers, arrays and strings
