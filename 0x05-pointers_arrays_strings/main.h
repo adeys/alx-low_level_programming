1740dc17d571340253ca985bbd367b94dc9a0adf
adeys <raphalg@duck.com>
2022-03-21 21:43:00 +0100

    Task 3: _puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif /* _MAIN_H */
