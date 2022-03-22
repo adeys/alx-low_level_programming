3925e0fb5e3ff248d7b76b6dbabdb707fcfde2d7
adeys <raphalg@duck.com>
2022-03-22 02:28:34 +0100

    Task 6: puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints one char out of 2 of a string
+ * @str: string to print the chars from
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int length, i;
+
+	length = 0;
+
+	while (str[len] != '\0')
+		length++;
+
+	for (i = 0; i < length; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
