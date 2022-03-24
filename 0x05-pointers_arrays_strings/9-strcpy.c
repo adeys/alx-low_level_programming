6b31ad3368495c9ebd3a8cb0a9da55dcb1de870b
adeys <raphalg@duck.com>
2022-03-24 19:06:05 +0100

    Fixed null byte added to dest in strcpy function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
+	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
-
-	dest[i] = '\0';
 
 	return (dest);
 }
