7aff43d192516fe944b519bb80aa4113afda928a
adeys <raphalg@duck.com>
2022-04-01 16:27:00 +0100

    Fixed task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,48 @@
 #include <errno.h>
 
 /**
+ * _isdigit - checks if a character is a digit
+ * @c: character to check
+ * Return: 1 if @c is a digit, 0 otherwise
+ */
+int _isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * _isnumeric - checks if a string contains valid numeric values
+ * @s: string to check
+ * Return: 1 if @s is valid, 0 otherwise
+ */
+int _isnumeric(char *s)
+{
+	int i, neg;
+
+	if (s[0] == '-')
+	{
+		neg = 1;
+		i = 1;
+	}
+	else
+	{
+		neg = 0;
+		i = 0;
+	}
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (!_isdigit(s[i]))
+			return (0);
+	}
+
+	if (i == 1 && neg)
+		return (0);
+	else
+		return (1);
+}
+
+/**
  * main - adds positive nuùbers
  * @argc: command line args count
  * @argv: command line args
@@ -10,31 +52,31 @@
  */
 int main(int argc, char **argv)
 {
-	int i, n, sum;
-	char *nptr, *endptr;
+	int i, sum;
+	char *nptr;
 
 	if (argc < 2)
 	{
 		printf("0\n");
+		return (0);
 	}
-	else
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
 	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
+		nptr = argv[i];
+		if (_isnumeric(nptr))
 		{
-			nptr = argv[i];
-			n = strtol(nptr, &endptr, 10);
-			if (nptr == endptr || errno != 0)
-			{
-				printf("Error\n");
-				return (1);
-			}
-
-			sum += n;
+			sum += atoi(nptr);
 		}
-
-		printf("%d\n", sum);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
+
+	printf("%d\n", sum);
 
 	return (0);
 }
