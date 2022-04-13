1e3b7c3b0a0248fe51e4467b8019bf2116b5d80a
adeys <raphalg@duck.com>
2022-04-13 09:52:50 +0100

    Task 0: print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: printer function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+
+	f(name);
+}
