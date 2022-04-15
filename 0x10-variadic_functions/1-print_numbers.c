27cde8b3aaba1b798eedf90864336271db033efc
adeys <raphalg@duck.com>
2022-04-15 19:01:58 +0100

    Fixed project diretory name

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: number separator
+ * @n: number count
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, sep;
+	va_list vl;
+
+	if (n == 0)
+		return;
+
+	va_start(vl, n);
+	sep = separator != NULL;
+
+	for (i = 0; i < n; i++)
+	{
+		if (sep && i != 0)
+			printf("%s", separator);
+
+		printf("%d", va_arg(vl, int));
+	}
+
+	va_end(vl);
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: string separator
+ * @n: string count
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, sep;
+	char *str;
+	va_list vl;
+
+	if (n == 0)
+		return;
+
+	va_start(vl, n);
+	sep = separator != NULL;
+
+	for (i = 0; i < n; i++)
+	{
+		if (sep && i != 0)
+			printf("%s", separator);
+
+		str = va_arg(vl, char *);
+		printf("%s", str != NULL ? str : "(nil)");
+	}
+
+	va_end(vl);
+	printf("\n");
+}
